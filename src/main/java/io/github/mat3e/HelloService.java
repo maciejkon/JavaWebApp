0e2df309e88772e6119780b25253923bbef9fcec
Maciej <maciejweltrowski@gmail.com>
2021-01-31 13:23:34 +0100

    POJO class, new layer with lang repository, con service-repository and service-servlet with another lang param

--- src/main/java/io/github/mat3e/HelloService.java
+++ src/main/java/io/github/mat3e/HelloService.java
@@ -5,7 +5,26 @@ import java.util.Optional;
 //this class will be prepare the response, because SRP (Single Responsibility Principle)
 class HelloService {
     static final String FALLBACK_NAME="world";
+    static final Lang FALLBACK_LANG=new Lang(1L,"Hello","en") ;
+
+    private LangRepository repository;
+
+    HelloService(){
+        this(new LangRepository());
+    }
+
+    HelloService(LangRepository repository) {
+        this.repository=repository;
+    }
+
     String prepareGreeting(String name) {
-        return "Hello " + Optional.ofNullable(name).orElse(FALLBACK_NAME) + "!";
+        return prepareGreeting(name,null);
+    }
+
+    String prepareGreeting(String name,String lang){
+        var langId = Optional.ofNullable(lang).map(Long::valueOf).orElse(FALLBACK_LANG.getId());
+        var welcomeMsg = repository.findById(langId).orElse(FALLBACK_LANG).getWelcomeMsg();
+        var nameToWelcome = Optional.ofNullable(name).orElse(FALLBACK_NAME);
+        return welcomeMsg + " " + nameToWelcome + "!";
     }
 }
